7b83488453a72e7c5488aaadee983917effa6901
Anton ROI <aroi@e3r4p7.unit.ua>
2018-05-05 20:34:03 +0300

    [Upd] - 99% ready

--- get_next_line.c
+++ get_next_line.c
@@ -6,63 +6,95 @@
 /*   By: aroi <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/17 16:47:45 by aroi              #+#    #+#             */
-/*   Updated: 2018/05/01 16:01:02 by aroi             ###   ########.fr       */
+/*   Updated: 2018/05/05 20:24:43 by aroi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "get_next_line.h"
 
-#include <stdio.h>
+static t_list	*ft_add_elem(int fd, t_list **list)
+{
+	char	*str;
+	t_list	*tmp;
 
-int		get_next_line(int fd, char **line)
+	if (!list || !*list)
+	{
+		str = ft_strdup("");
+		*list = ft_lstnew(str, 1);
+		ft_strdel(&str);
+		return (*list);
+	}
+	tmp = *list;
+	if (!tmp->next)
+	{
+		str = ft_strdup("");
+		tmp->next = ft_lstnew(str, 1);
+		ft_strdel(&str);
+	}
+	else
+		while (tmp->next)
+			tmp = tmp->next;
+	return (tmp);
+}
+
+static int		ft_read_until_end(char **line, char **buff, t_list **list_buff)
+{
+	char	*str;
+	t_list	*tmp;
+
+	tmp = *list_buff;
+	if ((*buff)[0] == '\0' && ((char *)tmp->content)[0] == '\0')
+	{
+		ft_strdel(buff);
+		return (0);
+	}
+	ft_strdel(line);
+	*line = ft_strjoin(tmp->content, *buff);
+	str = ft_strdup("");
+	tmp->next = ft_lstnew(str, 1);
+	ft_strdel(&str);
+	ft_strdel(buff);
+	return (1);
+}
+
+static void		ft_del_and_add_line(char **line, char **buff, char **lst_cont)
+{
+	char *str;
+
+	ft_strdel(line);
+	*line = ft_strsub(*lst_cont, 0, ft_strchr(*lst_cont, '\n') - *lst_cont);
+	str = *lst_cont;
+	*lst_cont = ft_strsub(*lst_cont, ft_strchr(*lst_cont, '\n') - *lst_cont + 1,
+		ft_strlen(*lst_cont) - (ft_strchr(*lst_cont, '\n') - *lst_cont) - 1);
+	ft_strdel(&str);
+	ft_strdel(buff);
+}
+
+int				get_next_line(int fd, char **line)
 {
 	char			*buff;
 	char			*str;
-	static t_list	*lst;
-	t_list			*lst_line;
+	static t_list	*list;
+	t_list			*list_buff;
 
-	buff = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
+	buff = (char *)malloc(BUFF_SIZE + 1);
 	if (fd < 0 || !buff || read(fd, buff, 0) == -1)
+	{
+		ft_strdel(&buff);
 		return (-1);
-	if (!lst)
-	{
-		str = ft_strdup("");
-		lst = ft_lstnew(str, 1);
-		ft_strdel(&str);
-		lst_line = lst;
 	}
-	else
-	{
-		if (!lst_line->next)
-		{
-			str = ft_strdup("");
-			lst_line->next = ft_lstnew(str, 1);
-			ft_strdel(&str);
-		}
-		else
-			lst_line = lst_line->next;
-	}
-	while (!ft_strchr((char *)lst_line->content, '\n'))
+	list_buff = ft_add_elem(fd, &list);
+	while (!ft_strchr(list_buff->content, '\n'))
 	{
 		ft_bzero(buff, BUFF_SIZE + 1);
 		read(fd, buff, BUFF_SIZE);
-		if (buff[0] == '\0' && !((char*)lst_line->content)[0])
-			return (0);
 		if (buff[0] == '\0')
-		{
-			*line = ft_strjoin((char *)lst_line->content, buff);
-			lst_line->content = "";
-			return (1);
-		}
-		str = (char *)lst_line->content;
-		lst_line->content = ft_strjoin((char *)lst_line->content, buff);
+			return (ft_read_until_end(line, &buff, &list_buff));
+		str = list_buff->content;
+		list_buff->content = ft_strjoin(list_buff->content, buff);
 		ft_strdel(&str);
 	}
-	*line = ft_strsub((char *)lst_line->content, 0, ft_strchr((char *)lst_line->content, '\n') - (char *)lst_line->content);
-	str = (char *)lst_line->content;
-	lst_line->content = ft_strsub((char *)lst_line->content, ft_strchr((char *)lst_line->content, '\n') - (char *)lst_line->content + 1, ft_strlen((char *)lst_line->content) - (ft_strchr((char *)lst_line->content, '\n') - (char *)lst_line->content) - 1);
-	ft_strdel(&str);
-	ft_strdel(&buff);
+	ft_del_and_add_line(line, &buff, (char **)&list_buff->content);
 	return (1);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aroi <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/17 16:42:37 by aroi              #+#    #+#             */
-/*   Updated: 2018/05/01 15:04:24 by aroi             ###   ########.fr       */
+/*   Updated: 2018/05/05 20:13:25 by aroi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,14 @@ int		main(int argc, char **argv)
 		{
 			fd = open(argv[i], O_RDONLY);
 			while ((cd = get_next_line(fd, &line)) > 0)
-				printf("Here's a line: %s and cd: %i\n", line, cd);
+			{
+				printf("%s\n", line);
+//				system("leaks a.out");
+	//			ft_strdel(&line);
+			}
 	//			printf(":)\n");
-	//		printf("Here's a line: %s and cd: %i\n", line, cd);
+			printf("Here's a line: %s and cd: %i\n", line, cd);
+//		ft_strdel(&line);
 		}
 //	system("leaks a.out");
 	return (0);
